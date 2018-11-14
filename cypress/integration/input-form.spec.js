09a4d68984e35fe306c1bae359b1edfaa3ec36b6
Vaughn Walters <vaughn@fortyau.com>
2018-11-14 13:54:03 -0600

    testing that li has correct data

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -17,9 +17,21 @@ describe("Input form", () => {
   }) 
   context('Form submission', () => {
     it.only('Adds a new todo on submit', () => {
+      
+      // use the following to stub an API response
+      cy.server()
+      cy.route('POST', '/api/todos', {
+        name: 'Buy eggs', 
+        id: 1,
+        isComplete: false
+      })
+      
       cy.get('.new-todo')
         .type('Buy eggs')
         .type('{enter}')
+      cy.get('.todo-list li')
+        .should('have.length', 1)
+        .and('contain', 'Buy Eggs')
     })
   })
 })
