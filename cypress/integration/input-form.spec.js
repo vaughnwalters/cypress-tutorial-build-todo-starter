144f7f8690910f548eb2a8a127170047c40101ae
Vaughn Walters <vaughn@fortyau.com>
2018-11-14 13:57:49 -0600

    refactored test list item into a const

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -15,23 +15,25 @@ describe("Input form", () => {
       .type(typedText)
       .should('have.value', typedText)
   }) 
+
   context('Form submission', () => {
     it.only('Adds a new todo on submit', () => {
-      
+      const itemText = 'Buy eggs'
+
       // use the following to stub an API response
       cy.server()
       cy.route('POST', '/api/todos', {
-        name: 'Buy eggs', 
+        name: 'itemText', 
         id: 1,
         isComplete: false
       })
       
       cy.get('.new-todo')
-        .type('Buy eggs')
+        .type('itemText')
         .type('{enter}')
       cy.get('.todo-list li')
         .should('have.length', 1)
-        .and('contain', 'Buy Eggs')
+        .and('contain', 'itemText')
     })
   })
 })
