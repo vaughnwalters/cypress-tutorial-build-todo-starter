d8c752d470916e44ea592d27a4281de2b4512931
Vaughn Walters <vaughn@fortyau.com>
2018-11-14 12:59:13 -0600

    setting up project and a couple of tests passing

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,20 @@
+describe("Input form", () => {
+  it("focuses input on load", () => {
+    cy.visit('http://localhost:3030')
+
+    cy.focused()
+      .should('have.class', 'new-todo')
+
+  })
+
+  it('accepts input', () => {
+    const typedText='Buy Milk'
+    cy.visit('http://localhost:3030')
+
+    cy.get('.new-todo')
+      .type(typedText)
+      .should('have.value', typedText)
+  }) 
+})
+
+
