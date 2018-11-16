ad4415c53a2edea87d9a5996d22f5c9fb29f1e35
Vaughn Walters <vaughn@fortyau.com>
2018-11-16 12:45:13 -0600

    creating app-init.spec test

---
+++ cypress/integration/app-init.spec.js
@@ -0,0 +1,29 @@
+const todos = [
+  {
+    "id": 1,
+    "name": "Buy Soylent",
+    "isComplete": false
+  },
+  {
+    "id": 2,
+    "name": "Drink Soylent",
+    "isComplete": false
+  },
+  {
+    "id": 3,
+    "name": "Buy more Soylent",
+    "isComplete": false
+  },
+  {
+    "id": 4,
+    "name": "Make French Toast",
+    "isComplete": false
+  }
+]
+
+describe('App initialization', () => {
+  it.only('Loads todos on page load', () => {
+    cy.server()
+    cy.route('GET', '/api/todos', todos )
+  })
+})
