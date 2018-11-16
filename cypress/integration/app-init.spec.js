77d7f1261ff3c3fb60fa0e25af9753869b4b8cd2
Vaughn Walters <vaughn@fortyau.com>
2018-11-16 14:47:11 -0600

    adding hardcoded stubbed todos

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -25,5 +25,9 @@ describe('App initialization', () => {
   it.only('Loads todos on page load', () => {
     cy.server()
     cy.route('GET', '/api/todos', todos )
+    cy.visit('/')
+
+    cy.get('.todo-list li')
+      .should('have.length', 4)
   })
 })
