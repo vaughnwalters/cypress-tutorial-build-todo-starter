cc1698e06170a2734f676ad0f73d9a93551848d9
Vaughn Walters <vaughn@fortyau.com>
2018-11-16 17:09:02 -0600

    creating custom command and another test to make sure that error message shows

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,10 +1,26 @@
 describe('App initialization', () => {
   it.only('Loads todos on page load', () => {
-    cy.server()
-    cy.route('GET', '/api/todos', 'fixture:todos' )
-    cy.visit('/')
+    cy.seedAndVisit()
 
     cy.get('.todo-list li')
       .should('have.length', 4)
   })
+
+  it.only('Displays an error on failure', () => {
+    cy.server()
+    cy.route({
+      url: '/api/todos', 
+      method: 'GET', 
+      status: 500, 
+      response: {}
+    })
+    cy.visit('/')
+
+    cy.get('.todo-list li')
+      .should('not.exist')
+
+    cy.get('.error')
+      .should('be.visible')
+  })
 })
+
