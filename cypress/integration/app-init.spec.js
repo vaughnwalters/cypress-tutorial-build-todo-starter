569fb3fc88dd5663c807e2920632238b7fe888f6
Vaughn Walters <vaughn@fortyau.com>
2018-11-16 16:43:10 -0600

    refactoring json data out into fixtures folder and then importing for test

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,30 +1,7 @@
-const todos = [
-  {
-    "id": 1,
-    "name": "Buy Soylent",
-    "isComplete": false
-  },
-  {
-    "id": 2,
-    "name": "Drink Soylent",
-    "isComplete": false
-  },
-  {
-    "id": 3,
-    "name": "Buy more Soylent",
-    "isComplete": false
-  },
-  {
-    "id": 4,
-    "name": "Make French Toast",
-    "isComplete": false
-  }
-]
-
 describe('App initialization', () => {
   it.only('Loads todos on page load', () => {
     cy.server()
-    cy.route('GET', '/api/todos', todos )
+    cy.route('GET', '/api/todos', 'fixture:todos' )
     cy.visit('/')
 
     cy.get('.todo-list li')
